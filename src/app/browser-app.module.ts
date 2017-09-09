dfea394cb94076d06ea6ac5aa04c4fc4605e1f5f
Julien Teyteau <--global>
2017-09-10 00:21:04 +0200

    add broken angular preboot support

--- src/app/browser-app.module.ts
+++ src/app/browser-app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserPrebootModule } from 'preboot/browser';
 import { AppModule } from './app.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   bootstrap: [ AppComponent ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'semper' }),
+    BrowserPrebootModule.replayEvents(),
     BrowserAnimationsModule,
     AppModule,
   ]
--- src/app/server-app.module.ts
+++ src/app/server-app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServerModule } from '@angular/platform-server';
+import { ServerPrebootModule } from 'preboot/server';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
@@ -10,11 +11,10 @@ import { AppComponent } from './app.component';
 @NgModule({
   bootstrap: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({
-      appId: 'semper'
-    }),
-    AppModule,
+    BrowserModule.withServerTransition({ appId: 'semper' }),
     ServerModule,
+    AppModule,
+    ServerPrebootModule.recordEvents({ appRoot: 'fi-root' }),
     NoopAnimationsModule,
     ModuleMapLoaderModule,
   ],
